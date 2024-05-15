d7829d0a0b0b2d5f6e0b9514dfff657d78480b64
carlosgiovani <carlosgiovani@gmail.com>
2024-05-15 16:05:05 -0300

    Cards Creature

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
   env: 'dev',
   urlApiSearch: 'https://api.magicthegathering.io/v1/sets?name=',
+  urlSetCreature: 'https://api.magicthegathering.io/v1/sets/ktk/booster',
   urlApi: 'https://api.magicthegathering.io/v1/cards',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   env: 'prod',
   urlApiSearch: 'https://api.magicthegathering.io/v1/sets?name=',
+  urlSetCreature: 'https://api.magicthegathering.io/v1/sets/ktk/booster',
   urlApi: 'https://api.magicthegathering.io/v1/cards',
 };
